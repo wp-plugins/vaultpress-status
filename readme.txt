12cc7892d1cba6dff2687b75346fcb04f5ab811d
ldebrouwer <ldebrouwer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-18 15:22:07 +0000

    Adding first version of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/vaultpress-status/trunk@374318 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== VaultPress Status ===
+Contributors: ldebrouwer
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=L5DXH3X8NNVNJ
+Tags: VaultPress, status, admin bar
+Requires at least: 3.1
+Tested up to: 3.1.1
+Stable tag: 0.1
+
+VaultPress bring your VaultPress backup status to your WordPress Admin Bar.
+
+== Description ==
+
+VaultPress bring your VaultPress backup status to your WordPress Admin Bar. Now you can enjoy that safe and secure feeling straight from the frontend of your website.
+
+== Installation ==
+
+1. Upload the folder `vaultpress-status` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. You're all done.
+
+== Frequently Asked Questions ==
+
+= It's not working! =
+
+You probably haven't installed VaultPress yet. Go check it out! http://www.vaultpress.com
+
+== Screenshots ==
+
+1. VaultPress Status in the Admin Bar.
+
+== Changelog ==
+
+= 0.1 =
+* First version of the plugin.
+
+== Upgrade Notice ==
---
+++ vaultpress-status.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Plugin Name: VaultPress Status
+ * Plugin URI: http://automattic.com/wordpress-plugins/
+ * Description: Adds VaultPress' status to your admin bar. To give you that nice and secured feeling, even in the front-end of your website.
+ * Version: 0.1
+ * Author: Luc De Brouwer
+ * Author URI: http://www.lucdebrouwer.nl/
+ *
+ */
+
+function LDB_VaultPress_Status_render() {
+	global $wp_admin_bar;
+	$ticker = json_decode(vaultpress_contact_service('ticker'));
+	$wp_admin_bar->add_menu( array(
+		'id' => 'LDB_VaultPress_status',
+		'title' => 'VaultPress Status',
+		'href' => '#'
+	) );
+	$wp_admin_bar->add_menu( array(
+	    'parent' => 'LDB_VaultPress_status',
+		'id' => 'LDB_VaultPress_Uploads',
+		'title' => '<div class="ldb_vaultpress_status_count">' . $ticker->uploads_progress . ' %</div> Uploads',
+		'href' => '#'
+	) );
+	$wp_admin_bar->add_menu( array(
+	    'parent' => 'LDB_VaultPress_status',
+		'id' => 'LDB_VaultPress_Plugins',
+		'title' => '<div class="ldb_vaultpress_status_count">' . $ticker->plugins_progress . ' %</div> Plugins',
+		'href' => '#'
+	) );
+	$wp_admin_bar->add_menu( array(
+	    'parent' => 'LDB_VaultPress_status',
+		'id' => 'LDB_VaultPress_Themes',
+		'title' => '<div class="ldb_vaultpress_status_count">' . $ticker->themes_progress . ' %</div> Themes',
+		'href' => '#'
+	) );
+	$wp_admin_bar->add_menu( array(
+	    'parent' => 'LDB_VaultPress_status',
+		'id' => 'LDB_VaultPress_Database',
+		'title' => '<div class="ldb_vaultpress_status_count">' . $ticker->tables_progress . ' %</div> Database',
+		'href' => '#'
+	) );
+}
+
+function LDB_VaultPress_Status_css() {
+?>
+	<style type="text/css">
+		#wpadminbar div.ldb_vaultpress_status_count {
+			display: block;
+			float: right;
+			line-height: 28px;			
+		}
+	</style>
+<?php
+}
+
+if( function_exists( 'vaultpress_contact_service' ) ) {
+	add_action( 'wp_head', 'LDB_VaultPress_Status_css' );
+	add_action( 'wp_before_admin_bar_render', 'LDB_VaultPress_Status_render' );
+}
